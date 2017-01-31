7123a3ab403ffa2b1c6ba2a106423e211be48db4
andela-tisrael <israel.tomilayo@andela.com>
2017-01-31 11:14:24 +0100

    Adding the filter template to the search Modal box

--- imports/plugins/included/ui-search/client/templates/searchModal/searchModal.js
+++ imports/plugins/included/ui-search/client/templates/searchModal/searchModal.js
@@ -5,6 +5,7 @@ import { Template } from "meteor/templating";
 import { i18next } from "/client/api";
 import { ProductSearch, Tags, OrderSearch, AccountSearch } from "/lib/collections";
 import { IconButton, SortableTable } from "/imports/plugins/core/ui/client/components";
+import { Session } from "meteor/session";
 
 /*
  * searchModal extra functions
@@ -43,10 +44,49 @@ Template.searchModal.onCreated(function () {
     }
   });
 
+    // Filters product by price
+  const priceFilter = (products, query) =>  {
+    return _.filter(products, (product) => {
+      if (product.price) {
+        const productMaxPrice = parseFloat(product.price.max);
+        const productMinPrice = parseFloat(product.price.min);
+        const queryMaxPrice = parseFloat(query[1]);
+        const queryMinPrice = parseFloat(query[0]);
+        if (productMinPrice >= queryMinPrice && productMaxPrice <= queryMaxPrice) {
+          return product;
+        }
+      }
+    });
+  };
+  // Sorts product by price
+  const sort = (products, type) => {
+    return products.sort((a, b) => {
+      const A = parseFloat(a.price.min);
+      const B = parseFloat(b.price.min);
+      if (A < B) {
+        return type === "DESC" ? 1 : -1;
+      }
+      if (A > B) {
+        return type === "ASC" ? 1 : -1;
+      }
+      return 0;
+    });
+  };
+
+
+  // Filters product by brand
+  function brandFilter(products, query) {
+    return _.filter(products, (product) => {
+      return product.vendor === query;
+    });
+  }
 
   this.autorun(() => {
     const searchCollection = this.state.get("searchCollection") || "products";
     const searchQuery = this.state.get("searchQuery");
+    const priceQuery = Session.get("priceFilter");
+    const brandQuery = Session.get("brandFilter");
+    const sortQuery = Session.get("sortValue");
     const facets = this.state.get("facets") || [];
     const sub = this.subscribe("SearchResults", searchCollection, searchQuery, facets);
 
@@ -55,7 +95,15 @@ Template.searchModal.onCreated(function () {
        * Product Search
        */
       if (searchCollection === "products") {
-        const productResults = ProductSearch.find().fetch();
+        let productResults = ProductSearch.find().fetch();
+        if (!["null", "all"].includes(priceQuery) && priceQuery) {
+          const range = priceQuery.split("-");
+          productResults =  priceFilter(productResults, range);
+        } if (!["null", "all"].includes(brandQuery) && brandQuery) {
+          productResults = brandFilter(productResults, brandQuery);
+        } if (sortQuery !== "null" && sortQuery) {
+          productResults = sort(productResults, sortQuery);
+        }
         const productResultsCount = productResults.length;
         this.state.set("productSearchResults", productResults);
         this.state.set("productSearchCount", productResultsCount);
@@ -183,6 +231,10 @@ Template.searchModal.events({
     $(".js-search-modal").delay(400).fadeOut(400, () => {
       Blaze.remove(view);
     });
+  },
+  "click [data-event-action=filterClick]": function () {
+    // alert("conclude");
+    $("#searchFilter").toggleClass("hidden");
   },
   "click [data-event-action=clearSearch]": function (event, templateInstance) {
     $("#search-input").val("");
