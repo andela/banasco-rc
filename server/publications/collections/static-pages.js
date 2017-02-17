bf1eec00129151d9268b4ccad6cee202ea2cd386
andela-fabolaji <femi.abolaji@andela.com>
2017-02-17 10:32:25 +0100

    create schema and publish collection

--- server/publications/collections/static-pages.js
+++ server/publications/collections/static-pages.js
@@ -2,6 +2,8 @@ import { Meteor } from "meteor/meteor";
 import { StaticPages } from "/lib/collections";
 
 Meteor.publish("StaticPages", function () {
-  if (this.userId === null) return this.ready();
+  if (this.userId === null) {
+    return this.ready();
+  }
   return StaticPages.find();
 });
